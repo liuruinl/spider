0c1726a9f60bcd6c63115c72a2ee368e987759d3
liurui <liuruinl@163.com>
2019-01-10 17:24:01 +0800

    add a new field

--- IronEgg/IronEgg/items.py
+++ IronEgg/IronEgg/items.py
@@ -9,20 +9,20 @@ import scrapy
 
 
 class IronEggItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	pass
 
 
 class Amazon(scrapy.Item):
-    _id=scrapy.Field()
-    RequestUrl=scrapy.Field()# 请求URL
-    SearchWords = scrapy.Field()# 搜索关键词
-
-    PageIndex = scrapy.Field()#当前页索引
-    #CurrentPageRank = scrapy.Field()#当前页面排名
-    TotalIndex = scrapy.Field()#总排名
-    ASIN = scrapy.Field()#查找目标
-
-    AddTime = scrapy.Field()#
-
+	_id = scrapy.Field()
+	RequestUrl = scrapy.Field()  # 请求URL
+	SearchWords = scrapy.Field()  # 搜索关键词
+	
+	PageIndex = scrapy.Field()  # 当前页索引
+	# CurrentPageRank = scrapy.Field()#当前页面排名
+	TotalIndex = scrapy.Field()  # 总排名
+	ASIN = scrapy.Field()  # 查找目标
+	IsAd = scrapy.Field()  # 是否广告
+	
+	AddTime = scrapy.Field()  #
--- IronEgg/IronEgg/spiders/amz.py
+++ IronEgg/IronEgg/spiders/amz.py
@@ -13,7 +13,7 @@ class AmzSpider(Spider):
     allowed_domains = ['www.amazon.com']
     DB_URI = 'mongodb://liurui:rootroot@172.17.0.2:27017/admin'
     DB_NAME = 'scrapydata'
-
+    
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
@@ -22,36 +22,36 @@ class AmzSpider(Spider):
         chrome_options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
         self.browser.set_page_load_timeout(120)
-
+        
         self.client = pymongo.MongoClient(self.DB_URI)
         self.db = self.client[self.DB_NAME]
         self.base_url = "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords="
         self.items = []
-
+    
     def close(self, reason):
         self.browser.close()
-
+    
     def start_requests(self):
         # todo reading key words from db here.
-
+        
         collection = self.db["words"]
-
+        
         start_urls = [
             # 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=net%20core%20in%20action'
         ]
-
+        
         for doc in collection.find({"owner": 1}):
             start_urls.append(self.base_url + doc["keys"])
-
+        
         for url in start_urls:
             yield Request(url=url, callback=self.parse)
-
+    
     def parse(self, response):
         if response.status != 200:
             return
         ul = response.css("div #atfResults").css("#s-results-list-atf").css("li")
         if ul.__len__() > 0:
-            #page = response.xpath('//*[@id="pagn"]/span[2]/text()').extract()
+            # page = response.xpath('//*[@id="pagn"]/span[2]/text()').extract()
             page = response.css("div #pagn span.pagnCur").xpath('text()').extract()
             for li in ul.css('li'):
                 if li.attrib.__len__() > 0 and 'data-asin' in li.attrib:
@@ -60,17 +60,21 @@ class AmzSpider(Spider):
                     amz["SearchWords"] = str(response.url).replace(self.base_url, "")  # 搜索关键词
                     amz["ASIN"] = li.attrib['data-asin']  # ASIN
                     amz["TotalIndex"] = li.attrib['id']  # 总排名
+                    amz["IsAd"] = False
+                    if li.attrib['class'] is not None and li.attrib['class'] != "" and str(li.attrib['class']).find(
+                            "AdHolder") >= 0:
+                        amz["IsAd"] = True
                     if page.__len__() > 0:
                         amz["PageIndex"] = page[0]  # 当前页索引
                         yield amz
-            if page.__len__() > 0 and int(page[0]) < 20:
+            if page.__len__() > 0 and int(page[0]) < 1:  # 只取20页
                 le = LinkExtractor(restrict_css="#pagnNextLink")
                 links = le.extract_links(response)
                 if links:
                     next_url = links[0].url
                     # time.sleep(1)
                     yield scrapy.Request(next_url, callback=self.parse, dont_filter=True)
-
+    
     def parse_1(self, response):
         page = 1
         varies = [
@@ -93,7 +97,7 @@ class AmzSpider(Spider):
                 amz["Title"] = attr["title"]
             if 'href' in attr:
                 amz["Href"] = attr["href"]
-
+            
             if amz["Href"] != "":
                 amz['KeyWords'] = "a"
                 amz["Href"].split('/dp/')
