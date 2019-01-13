0accbd540a68440ed47afb8f1f62f334144c449b
liuruinl <liuruinl@163.com>
2019-01-13 23:49:10 +0800

    agent opt

--- IronEgg/IronEgg/middlewares.py
+++ IronEgg/IronEgg/middlewares.py
@@ -12,46 +12,46 @@ class IroneggSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
-    
+
     @classmethod
     def from_crawler(cls, crawler):
         # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
-    
+
     def process_spider_input(self, response, spider):
         # Called for each response that goes through the spider
         # middleware and into the spider.
-        
+
         # Should return None or raise an exception.
         return None
-    
+
     def process_spider_output(self, response, result, spider):
         # Called with the results returned from the Spider, after
         # it has processed the response.
-        
+
         # Must return an iterable of Request, dict or Item objects.
         for i in result:
             yield i
-    
+
     def process_spider_exception(self, response, exception, spider):
         # Called when a spider or process_spider_input() method
         # (from other spider middleware) raises an exception.
-        
+
         # Should return either None or an iterable of Response, dict
         # or Item objects.
         pass
-    
+
     def process_start_requests(self, start_requests, spider):
         # Called with the start requests of the spider, and works
         # similarly to the process_spider_output() method, except
         # that it doesn’t have a response associated.
-        
+
         # Must return only requests (not items).
         for r in start_requests:
             yield r
-    
+
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
@@ -60,18 +60,18 @@ class IroneggDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
-    
+
     @classmethod
     def from_crawler(cls, crawler):
         # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
-    
+
     def process_request(self, request, spider):
         # Called for each request that goes through the downloader
         # middleware.
-        
+
         # Must either:
         # - return None: continue processing this request
         # - or return a Response object
@@ -79,26 +79,26 @@ class IroneggDownloaderMiddleware(object):
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
         return None
-    
+
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
-        
+
         # Must either;
         # - return a Response object
         # - return a Request object
         # - or raise IgnoreRequest
         return response
-    
+
     def process_exception(self, request, exception, spider):
         # Called when a download handler or a process_request()
         # (from other downloader middleware) raises an exception.
-        
+
         # Must either:
         # - return None: continue processing this exception
         # - return a Response object: stops process_exception() chain
         # - return a Request object: stops process_exception() chain
         pass
-    
+
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
@@ -113,7 +113,6 @@ class SeleniumMiddleware(object):
         if spider.name == 'amz':
             try:
                 print(request.url)
-                #spider.browser.get("http://myip.ipip.net/")
                 spider.browser.get(request.url)
                 spider.browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
             except TimeoutException as e:
@@ -128,7 +127,7 @@ import random
 
 
 class CustomHttpProxyMiddleware(object):
-    
+
     def process_request(self, request, spider):
         # TODO implement complex proxy providing algorithm
         if self.use_proxy(request):
@@ -139,7 +138,7 @@ class CustomHttpProxyMiddleware(object):
                 pass
             # log.msg("Exception %s" % e, _level=log.CRITICAL)
             # log.critical("Exception %s" % e)
-    
+
     def use_proxy(self, request):
         """
         using direct download for depth <= 2
@@ -152,10 +151,11 @@ class CustomHttpProxyMiddleware(object):
         return True
 
 
-class CustomUserAgentMiddleware(object):
-    def process_request(self, request, spider):
-        agent = random.choice(AGENTS_ALL)
-        request.headers['User-Agent'] = agent
+# class CustomUserAgentMiddleware(object):
+# def process_request(self, request, spider):
+# agent = random.choice(AGENTS_ALL)
+# spider.set_driver(agent)
+# request.headers['User-Agent'] = agent
 
 
 '''
--- IronEgg/IronEgg/settings.py
+++ IronEgg/IronEgg/settings.py
@@ -25,7 +25,7 @@ NEWSPIDER_MODULE = 'IronEgg.spiders'
 # USER_AGENT = 'IronEgg (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
@@ -33,7 +33,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -61,9 +61,9 @@ COMMANDS_MODULE = 'IronEgg.commands'
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     #    'IronEgg.middlewares.IroneggDownloaderMiddleware': 543,
-    'IronEgg.middlewares.CustomUserAgentMiddleware': 543,
+    # 'IronEgg.middlewares.CustomUserAgentMiddleware': 543,
     'IronEgg.middlewares.SeleniumMiddleware': 544,
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  # 这里要设置原来的scrapy的useragent为None，否者会被覆盖掉
+    # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,  # 这里要设置原来的scrapy的useragent为None，否者会被覆盖掉
 }
 
 # Enable or disable extensions
